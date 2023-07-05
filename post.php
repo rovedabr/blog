849083b02c379e3a292acfd178965937862bec87
rovedabr <rovedabr@gmail.com>
2023-07-05 16:25:15 -0300

    páginas de post - CSS ok

--- post.php
+++ post.php
@@ -22,28 +22,29 @@
     <div class="img-container">
       <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
     </div>
-    <div class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae non eum officia praesentium eligendi quidem similique ut beatae ab quibusdam provident, corporis maiores nam cumque, cum, possimus rerum exercitationem.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae non eum officia praesentium eligendi quidem similique ut beatae ab quibusdam provident, corporis maiores nam cumque, cum, possimus rerum exercitationem.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae non eum officia praesentium eligendi quidem similique ut beatae ab quibusdam provident, corporis maiores nam cumque, cum, possimus rerum exercitationem.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae non eum officia praesentium eligendi quidem similique ut beatae ab quibusdam provident, corporis maiores nam cumque, cum, possimus rerum exercitationem.
+    <div class="post-content">
+      <p id="text-container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae non eum officia praesentium eligendi quidem similique ut beatae ab quibusdam provident, corporis maiores nam cumque, cum, possimus rerum exercitationem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae non eum officia praesentium eligendi quidem similique ut beatae ab quibusdam provident, corporis maiores nam cumque, cum, possimus rerum exercitationem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae non eum officia praesentium eligendi quidem similique ut beatae ab quibusdam provident, corporis maiores nam cumque, cum, possimus rerum exercitationem.
+      </p>
     </div>
   </div>
+    <aside id="nav-container">
+      <h3 id="tag-title">Tags</h3>
+        <ul id="tag-list">
+          <?php foreach($currentPost['tags'] as $tag): ?>
+            <li><a href=""><?= $tag ?></a></li>
+          <?php endforeach; ?> 
+        </ul>
+      <h3 id="categories-title">Categorias</h3>
+      <ul id="categories-list">
+        <?php foreach($categories as $category): ?>
+          <li><a href=""><?= $category ?></a></li>
+        <?php endforeach; ?> 
+      </ul>   
+    </aside>
 </main>
-<aside id="nav-container">
-  <h3 id="tags-title">Tags</h3>
-    <ul class="tag-list">
-      <?php foreach($currentPost['tags'] as $tag): ?>
-        <li><a href=""><?= $tag ?></a></li>
-      <?php endforeach; ?> 
-    </ul>
-  <h3 id="categories-title">Categorias</h3>
-  <ul id="categories-list">
-    <?php foreach($categories as $category): ?>
-      <li><a href=""><?= $category ?></a></li>
-    <?php endforeach; ?> 
-  </ul>   
-</aside>
-
 <?php
   include_once("templates/footer.php")
 ?>
